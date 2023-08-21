bb1159474ab7d87fa2e9f32862bc0debd8b46b1f
Zeegaths <zarahgathoni76@gmail.com>
2023-08-21 22:34:21 +0300

    show

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- This script shows a full description of the table
-CREATE TABLE IF NOT EXISTS first_table IN hbtn_0c_0;
-DESC first_table FROM hbtn_0c_0;
+SHOW CREATE TABLE hbtn_0c_0.first_table;
