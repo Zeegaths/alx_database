a9edf630ba48b388303e4f2b3e5cf0767c9b03a2
Zeegaths <zarahgathoni76@gmail.com>
2023-08-21 22:07:00 +0300

    cong

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,8 @@
 -- This script shows a full description of the table
 SET @query = (
-  SELECT CONCAT(
-    'SHOW CREATE TABLE `', TABLE_NAME, '`;'
+  SELECT GROUP_CONCAT(
+    'SHOW CREATE TABLE `', TABLE_NAME, '`'
+    SEPARATOR ' UNION ALL '
   )
   FROM INFORMATION_SCHEMA.TABLES
   WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
