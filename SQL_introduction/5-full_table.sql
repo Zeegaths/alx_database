1ef12d4597922382049f5d574fd4ba182c42fbf1
Zeegaths <zarahgathoni76@gmail.com>
2023-08-21 22:08:23 +0300

    set

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,14 @@
 -- This script shows a full description of the table
-SET @query = (
-  SELECT GROUP_CONCAT(
-    'SHOW CREATE TABLE `', TABLE_NAME, '`'
-    SEPARATOR ' UNION ALL '
-  )
-  FROM INFORMATION_SCHEMA.TABLES
-  WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
-);
+SELECT
+    GROUP_CONCAT(
+        COLUMN_NAME, ' ',
+        COLUMN_TYPE,
+        IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
+        IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
+        IF(COLUMN_KEY = 'PRI', ' PRIMARY KEY', ''),
+        IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')
+        ORDER BY ORDINAL_POSITION
+        SEPARATOR ',\n  '
+    ) AS 'Create Table'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
