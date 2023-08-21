47358675329ffa9823b1842cba627a4a578486a8
Zeegaths <zarahgathoni76@gmail.com>
2023-08-21 22:05:13 +0300

    CONCAT

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,8 @@
 -- This script shows a full description of the table
 SET @query = (
   SELECT CONCAT(
-    'SHOW CREATE TABLE ', TABLE_NAME, ';'
+    'SHOW CREATE TABLE `', TABLE_NAME, '`;'
   )
   FROM INFORMATION_SCHEMA.TABLES
-  WHERE TABLE_SCHEMA = hbtn_0c_0 AND TABLE_NAME = first_table
+  WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
 );
