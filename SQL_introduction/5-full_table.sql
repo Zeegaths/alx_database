ea01b83cf341e09d85c0c3dee9b9d866eb4d9148
Zeegaths <zarahgathoni76@gmail.com>
2023-08-21 22:03:14 +0300

    CHAT

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,8 @@
 -- This script shows a full description of the table
-SHOW TABLE IF EXISTS first_table FROM hbtn_0c_0;
+SET @query = (
+  SELECT CONCAT(
+    'SHOW CREATE TABLE ', TABLE_NAME, ';'
+  )
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_SCHEMA = hbtn_0c_0 AND TABLE_NAME = first_table
+);
