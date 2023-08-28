eaa8e239b4d83a2b296cb56d53375be0cb860ff5
Zeegaths <zarahgathoni76@gmail.com>
2023-08-28 16:12:43 +0300

    USERS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
 -- This query shows privileges granted for the two users
+CREATE user_0d_1 'user_0d_1'@'hostname';
+CREATE user_0d_1 'user_0d_2'@'hostname';
+
 SHOW GRANTS FOR 'user_0d_1'@'hostname';
 SHOW GRANTS FOR 'user_0d_2'@'hostname';
