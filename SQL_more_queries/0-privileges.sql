27ec14a6f1d8e61565409986e687ed068acdf42a
Zeegaths <zarahgathoni76@gmail.com>
2023-08-28 16:19:16 +0300

    localhost

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- This query shows privileges granted for the two users
-SHOW GRANTS FOR 'user_0d_1'@'hostname';
-SHOW GRANTS FOR 'user_0d_2'@'hostname';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
