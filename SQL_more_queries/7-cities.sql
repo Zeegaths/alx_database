a3555c7cdcef752a73290f2c7861e52596e0e546
Zeegaths <zarahgathoni76@gmail.com>
2023-08-28 17:42:21 +0300

    cities and states

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- creates table, database . state is a foreigh key referencing id of the states table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL,state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id));
