73dc0b63427b4703c6fe3859bde0dc4fc2c84d92
Zeegaths <zarahgathoni76@gmail.com>
2023-08-28 17:56:37 +0300

    cities of carlifonia

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- lists cities and states in carlifonia
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+    AND states.name = 'California'
+ORDER BY cities.id ASC;
