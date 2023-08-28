315b6a81e41e7f3d662464d31b2cf1a44e1f78d6
Zeegaths <zarahgathoni76@gmail.com>
2023-08-28 16:37:19 +0300

    flush

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- These queries create user and grants all priviledges
 CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+FLUSH PRIVILEGES;
